cf01329470b13e2d87b77a6489b9ea40cfe7a3ef
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2023-01-21 22:41:42 -0500

    first pass on services

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,13 +6,11 @@ const withStyles = makeStyles(() => ({
         display: "flex",
         flexDirection: "column",
         background: "#00704a",
-        height: "500px",
         padding: "5% 14% 5% 14%",
         margin: "auto"
     },
     servicesHeaderTextWrapper: {
         display: "flex",
-        margin: "0px 0 50px 0",
         justifyContent: "center",
         color: "black"
     },
@@ -28,11 +26,21 @@ const withStyles = makeStyles(() => ({
         }
     },
     servicesDescriptionWrapper: {
-
+        textAlign: "left"
     },
     servicesParagraph: {
         lineHeight: 2,
         color: "white"
+    },
+    servicesListUl: {
+        color: "white",
+        textAlign: "left",
+        "& li": {
+            margin: "15px 0"
+        }
+    },
+    servicesListWrapper: {
+        display: "flex"
     }
 
 }));
@@ -42,29 +50,31 @@ const Services = () => {
     return (
         <div className={classes.servicesWrapper}>
             <div className={classes.servicesHeaderTextWrapper}>
-                <Typography className={classes.servicesHeader}>Services</Typography>
+                <Typography className={classes.servicesHeader}>Our Services</Typography>
             </div>
             <div className={classes.servicesDescriptionWrapper}>
                 <p className={classes.servicesParagraph}>Many landscape companies provide select components of landscape services. Some mow lawns, some install walks and others just sell plants.
                     What makes Clover Lawn & Landscape unique is the ability to encompass all of your landscape needs under one company's management.
                     This helps to eliminate the need of coordinating three or four various companies to maintain your property. So from landscape design to plant selection
-                    to construction to maintenance services, we've got your project covered; one call does it all. Our services include:
+                    to construction to maintenance services, we've got your project covered; one call does it all.
                 </p>
             </div>
-            <div> Services list - TODO
-                <p>
-                    Custom landscape design
-                    Complete landscape renovations
-                    Installations of walkways and patios (flagstone and all styles of pavers)
-                    Retaining walls of all styles
-                    Landscape lighting
-                    Expert landscape installation
-                    Custom water features
-                    Sod installation Gardening and maintenance
-                    Complete tree service
-                    Fully staffed retail nursery and garden center
-                    Grading and drainage systems
-                </p>
+            <div className={classes.servicesListWrapper}>
+                <ul className={classes.servicesListUl}>
+                    <li>Custom landscape design</li>
+                    <li>Complete landscape renovations</li>
+                    <li>Installations of walkways and patios (flagstone and all styles of pavers)</li>
+                    <li>Retaining walls of all styles</li>
+                    <li>Landscape lighting</li>
+                    <li>Expert landscape installation</li>
+                </ul>
+                <ul className={classes.servicesListUl}>
+                    <li>Custom water features</li>
+                    <li>Sod installation Gardening and maintenance</li>
+                    <li>Complete tree service</li>
+                    <li>Fully staffed retail nursery and garden center</li>
+                    <li>Grading and drainage systems</li>
+                </ul>
             </div>
         </div>
     )
