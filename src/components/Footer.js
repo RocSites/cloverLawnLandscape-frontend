2898487029a8bfc3637b4214dd0446e5d96b55d4
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2023-08-09 15:47:32 -0400

    added address and phone number to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,22 @@ const withStyles = makeStyles(() => ({
     footerWrapper: {
         backgroundColor: "#00704a",
         height: "75px",
+        padding:"10px 0",
         display: "flex",
+        flexDirection: "column",
         justifyContent: "flex-start",
-        alignItems: "center"
+        alignItems: "left"
     },
     cloverCopyright: {
         color: "white",
         textAlign: "left",
         marginLeft: "15px"
+    },
+    footerPhone: {
+        textAlign: "left",
+        padding: "10px 0",
+        marginLeft: "15px",
+        color: "white"
     }
 
 }))
@@ -23,6 +31,8 @@ const Footer = () => {
     return (
         <div className={classes.footerWrapper}>
             <Typography className={classes.cloverCopyright}> &copy; {new Date().getFullYear()} Clover Lawn & Landscape. All Rights Reserved.</Typography>
+            <Typography className={classes.cloverCopyright}>485 N Landing Rd North, Rochester, NY 14625</Typography>
+            <a className={classes.footerPhone} href="tel:(585) 244-1626">(585) 244-1626</a>
         </div>
     )
 }
