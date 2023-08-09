9887fd39f80a13938126bb9b5b4fa03017a02d3c
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2023-08-09 16:21:40 -0400

    updated rocsites name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -36,7 +36,7 @@ const Footer = () => {
             <Typography className={classes.cloverCopyright}> &copy; {new Date().getFullYear()} Clover Lawn & Landscape. All Rights Reserved.</Typography>
             <Typography className={classes.cloverCopyright}>485 N Landing Rd North, Rochester, NY 14625</Typography>
             <a className={classes.footerPhone} href="tel:(585) 244-1626">(585) 244-1626</a>
-            <p className={classes.rocsitesText}>Website created and maintained by <span><a href="https://www.rocsites.com/" target="_blank">Rocsites</a></span></p>
+            <p className={classes.rocsitesText}>Website created and maintained by <span><a href="https://www.rocsites.com/" target="_blank">RocSites</a></span></p>
         </div>
     )
 }
